c80b1df6db23e677c679482324de8bc0b93c7bc5
Animeshpatra2001 <147701997+Animeshpatra2001@users.noreply.github.com>
2024-05-16 15:52:50 +0530

    Add files via upload

---
+++ Assignment3/Q06_TreeSetOperations.java
@@ -0,0 +1,44 @@
+/*6. Write a program to create a TreeSet of Integer type and perform the below operation on it.
+(a.) Display the TreeSet
+(b.) Ask the user to enter a number and search that number is it present in the list or not.
+(c.) Remove an element from tree.*/
+package genericsCollection;
+
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class Q06_TreeSetOperations {
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+
+		TreeSet<Integer> treeSet = new TreeSet<>();
+
+		treeSet.add(5);
+		treeSet.add(10);
+		treeSet.add(15);
+		treeSet.add(20);
+		treeSet.add(25);
+
+		System.out.println("TreeSet: " + treeSet);
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter a number to search: ");
+		int searchNumber = sc.nextInt();
+
+		if (treeSet.contains(searchNumber)) {
+			System.out.println("Number " + searchNumber + " is present in the TreeSet.");
+		} else {
+			System.out.println("Number " + searchNumber + " is not present in the TreeSet.");
+		}
+
+		System.out.print("Enter a number to remove: ");
+		int removeNumber = sc.nextInt();
+		if (treeSet.remove(removeNumber)) {
+			System.out.println("Number " + removeNumber + " removed from the TreeSet.");
+			System.out.println("Updated TreeSet: " + treeSet);
+		} else {
+			System.out.println("Number " + removeNumber + " is not present in the TreeSet.");
+		}
+	}
+}
